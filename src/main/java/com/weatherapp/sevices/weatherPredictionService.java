75038dfae68a3bfa48c754ae972268bcf4567d3c
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:07:31 +0400

    WeatherPredictionService

--- src/main/java/com/weatherapp/sevices/weatherPredictionService.java
+++ src/main/java/com/weatherapp/sevices/weatherPredictionService.java
@@ -4,15 +4,15 @@ import lombok.Value;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class weatherPredictionService {
 
 
     private final OpenAiChatClient openAiChatClient;
 
-    public WeatherPredictionService (@Value ("${spring.ai.openai.api-key}") String apiKey){
-
+    public WeatherPredictionService (@Value ("${}") String apiKey){
+        this.openAiChatClient = new OpenAiChatClient(apiKey);
+    }
 
 
 
