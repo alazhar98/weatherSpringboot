f210dc07e0b2c5a28fe9f87674e8d013256c2fe1
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:51:15 +0400

    Add service dependencies to WeatherController

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -1,10 +1,19 @@
 package com.weatherapp.Controllers;
-
+import com.weatherapp.sevices.WeatherService;
+import com.weatherapp.sevices.weatherPredictionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/weather")
 public class weatherController {
+
+    @Autowired
+    public WeatherService weatherService;
+
+    @Autowired
+    private weatherPredictionService weatherPredictionService;
+
 
 }
