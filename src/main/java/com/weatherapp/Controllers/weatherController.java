4b99e588224dfcdffd5b26df4485c37becbf5d6c
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:53:34 +0400

    Add endpoint method for fetching weather by city

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -1,7 +1,9 @@
 package com.weatherapp.Controllers;
+import com.weatherapp.Models.weatherResponse;
 import com.weatherapp.sevices.WeatherService;
 import com.weatherapp.sevices.weatherPredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,10 @@ public class weatherController {
 
     @Autowired
     private weatherPredictionService weatherPredictionService;
+
+    public weatherResponse getWeather(@PathVariable String city){
+        return weatherService.getWeather(city);
+    }
 
 
 }
