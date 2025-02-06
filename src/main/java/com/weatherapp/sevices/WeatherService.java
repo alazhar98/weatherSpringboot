50fd2b75ca9dba9a2ba77cf1cb75edc8a321b188
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 09:59:41 +0400

    get weather service

--- src/main/java/com/weatherapp/sevices/WeatherService.java
+++ src/main/java/com/weatherapp/sevices/WeatherService.java
@@ -1,4 +1,19 @@
 package com.weatherapp.sevices;
 
+import com.weatherapp.Models.weatherResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class WeatherService {
+    private static final String API_KEY = "<>";
+    private static final String URL = "";
+
+    public weatherResponse getWeather(String city){
+        String endPoint= String.format(URL,city,API_KEY);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(endPoint,weatherResponse.class);
+    }
+
+
 }
