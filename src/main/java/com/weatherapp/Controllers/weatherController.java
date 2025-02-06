d565b8fd2f328b01f457f1b44f7ffb3c3c6e25a2
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:42:35 +0400

    Add RestController and RequestMapping annotations

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -1,4 +1,10 @@
 package com.weatherapp.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
 public class weatherController {
+
 }
