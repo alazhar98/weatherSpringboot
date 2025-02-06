e7ff64a07aa7b7ea47fc0706c4d16fc7b9171e20
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:01:38 +0400

    OpenAiChatClient

--- src/main/java/com/weatherapp/sevices/weatherPredictionService.java
+++ src/main/java/com/weatherapp/sevices/weatherPredictionService.java
@@ -1,4 +1,22 @@
 package com.weatherapp.sevices;
 
+import lombok.Value;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.weatherapp.Models.weatherResponse;
+@Service
 public class weatherPredictionService {
+
+    @Value("${openweathermap.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public weatherResponse getWeather(String city){
+        private final OpenAiChatClient openAiChatClient;
+
+
+    }
+
 }
