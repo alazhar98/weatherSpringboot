a80c7e1b63a26bda6f7cf44722d2be1a7c83d44b
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-06 10:03:06 +0400

    delete some of the code that unused

--- src/main/java/com/weatherapp/sevices/weatherPredictionService.java
+++ src/main/java/com/weatherapp/sevices/weatherPredictionService.java
@@ -5,16 +5,19 @@ import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.weatherapp.Models.weatherResponse;
+
 @Service
 public class weatherPredictionService {
 
-    @Value("${openweathermap.api.key}")
+
     private String apiKey;
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final OpenAiChatClient openAiChatClient;
 
     public weatherResponse getWeather(String city){
-        private final OpenAiChatClient openAiChatClient;
+
+
 
 
     }
