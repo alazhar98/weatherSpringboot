4c02f600db118d434fc2b8944a4e49254a0fcdd4
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-09 14:28:08 +0400

    Refactor WeatherPredictionService for cleaner code

--- src/main/java/com/weatherapp/sevices/WeatherPredictionService.java
+++ src/main/java/com/weatherapp/sevices/WeatherPredictionService.java
@@ -1,19 +1,20 @@
 package com.weatherapp.sevices;
 
 import com.weatherapp.Models.WeatherForecastResponse;
-import org.springframework.ai.openai.OpenAiChatClient;
-import org.springframework.ai.openai.api.OpenAiApi;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
+
 
 @Service
 public class WeatherPredictionService {
+
+    @Value( "${spring.ai.openai.api-key}")
     private final String apiKey = "spring.ai.openai.api-key"; // Replace with your OpenWeather API key
     private final String apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={apiKey}";
 
-    private final RestTemplate restTemplate;
+    public final RestTemplate restTemplate;
 
     public WeatherPredictionService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -21,8 +22,7 @@ public class WeatherPredictionService {
 
     public WeatherForecastResponse getWeatherForecast(String city) {
         String url = apiUrl.replace("{city}", city).replace("{apiKey}", apiKey);
-        WeatherForecastResponse forecastResponse = restTemplate.getForObject(url, WeatherForecastResponse.class);
-        return forecastResponse;
+        return restTemplate.getForObject(url, WeatherForecastResponse.class);
     }
 }
 
