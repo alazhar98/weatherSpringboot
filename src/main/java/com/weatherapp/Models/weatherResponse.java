b5e8652f3c61050c19fbcca4cd1cb78d236c0786
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-09 11:45:27 +0400

    Add Weather array to WeatherResponse model

--- src/main/java/com/weatherapp/Models/weatherResponse.java
+++ src/main/java/com/weatherapp/Models/weatherResponse.java
@@ -7,12 +7,20 @@ import lombok.Data;
 public class weatherResponse {
     private Main main ;
     private String name ;
+    private Weather[] weather;
 
     @Data
     public static class Main{
         private double temp ;
         private double humidity ;
         private double pressure ;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Weather {
+        private String main;
+        private String description;
     }
 
 }
