85854f5455ad4d420605742bdf8de8417637251c
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-09 11:44:15 +0400

    Add WeatherForecastResponse model for API data parsing

---
+++ src/main/java/com/weatherapp/Models/WeatherForecastResponse.java
@@ -0,0 +1,32 @@
+package com.weatherapp.Models;
+
+import lombok.Data;
+
+import java.util.List;
+
+public class WeatherForecastResponse {
+
+    private List<Forecast> list;
+
+    @Data
+    public static class Forecast {
+        private long dt;
+        private Main main;
+        private Weather[] weather;
+        private String dt_txt;
+    }
+
+    @Data
+    public static class Main {
+        private double temp;
+        private double humidity;
+        private double pressure;
+    }
+
+    @Data
+    public static class Weather {
+        private String main;
+        private String description;
+    }
+
+}
