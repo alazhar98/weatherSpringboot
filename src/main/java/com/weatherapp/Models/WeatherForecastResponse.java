a9cf5807859477eb73882d7633f121e2cdbede47
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-09 12:35:29 +0400

    Refactor WeatherForecastResponse to add getters and setters

--- src/main/java/com/weatherapp/Models/WeatherForecastResponse.java
+++ src/main/java/com/weatherapp/Models/WeatherForecastResponse.java
@@ -1,32 +1,73 @@
 package com.weatherapp.Models;
 
-import lombok.Data;
-
 import java.util.List;
 
 public class WeatherForecastResponse {
 
-    private List<Forecast> list;
+    private List<Forecast> list; // List of forecast objects
 
-    @Data
+    public List<Forecast> getList() {
+        return list;
+    }
+
+    public void setList(List<Forecast> list) {
+        this.list = list;
+    }
+
     public static class Forecast {
-        private long dt;
-        private Main main;
-        private Weather[] weather;
-        private String d;
+
+        private String dt_txt; // Date and time of forecast
+        private Main main; // Main weather data (temperature, etc.)
+        private Weather[] weather; // Weather descriptions
+
+        public String getDt_txt() {
+            return dt_txt;
+        }
+
+        public void setDt_txt(String dt_txt) {
+            this.dt_txt = dt_txt;
+        }
+
+        public Main getMain() {
+            return main;
+        }
+
+        public void setMain(Main main) {
+            this.main = main;
+        }
+
+        public Weather[] getWeather() {
+            return weather;
+        }
+
+        public void setWeather(Weather[] weather) {
+            this.weather = weather;
+        }
     }
 
-    @Data
     public static class Main {
-        private double temp;
-        private double humidity;
-        private double pressure;
+
+        private double temp; // Temperature
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
     }
 
-    @Data
     public static class Weather {
-        private String main;
-        private String description;
-    }
 
+        private String description; // Weather description
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
 }
