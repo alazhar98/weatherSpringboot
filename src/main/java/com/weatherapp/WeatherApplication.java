aa6861c63cc50ff0d940c018c2aad9b1326811a8
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-10 12:04:57 +0400

    Fix indentation in WeatherApplication main method

--- src/main/java/com/weatherapp/WeatherApplication.java
+++ src/main/java/com/weatherapp/WeatherApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(WeatherApplication.class, args);
+        SpringApplication.run(WeatherApplication.class, args);
 
-	}
+    }
 }
 
