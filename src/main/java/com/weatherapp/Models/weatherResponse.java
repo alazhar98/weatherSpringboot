265936a94e3702f3b4abf4b01e3bf3be04ae3dcc
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-10 14:18:51 +0400

    Add timezone-based formatting for sunrise and sunset times

--- src/main/java/com/weatherapp/Models/weatherResponse.java
+++ src/main/java/com/weatherapp/Models/weatherResponse.java
@@ -1,5 +1,6 @@
 package com.weatherapp.Models;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,6 +18,7 @@ public class weatherResponse {
     private String name;
     private Weather[] weather;
     private Sys sys;
+    private static Coord coord;
 
     @Data
     public static class Main {
@@ -30,8 +32,8 @@ public class weatherResponse {
     public static class Weather {
         private String main;
         private String description;
-
     }
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Sys {
@@ -41,20 +43,34 @@ public class weatherResponse {
         @JsonIgnore
         private long sunset;
 
+        private String country;
+
         @JsonProperty("sunriseTime")
         public String getSunriseTime() {
-            return formatUnixTimestamp(sunrise);
+            return formatUnixTimestamp(sunrise, coord.getLat(), coord.getLon());
         }
 
         @JsonProperty("sunsetTime")
         public String getSunsetTime() {
-            return formatUnixTimestamp(sunset);
+            return formatUnixTimestamp(sunset, coord.getLat(), coord.getLon());
         }
 
-        private String formatUnixTimestamp(long timestamp) {
+        private String formatUnixTimestamp(long timestamp, double lat, double lon) {
+            String timezone = getTimeZone(lat, lon);
             ZonedDateTime dateTime = Instant.ofEpochSecond(timestamp)
-                    .atZone(ZoneId.systemDefault());
+                    .atZone(ZoneId.of(timezone));
             return dateTime.format(DateTimeFormatter.ofPattern("hh:mm a"));
         }
+
+        private String getTimeZone(double lat, double lon) {
+            return ZoneId.of("GMT").getId();
+        }
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Coord {
+        private double lat;
+        private double lon;
     }
 }
