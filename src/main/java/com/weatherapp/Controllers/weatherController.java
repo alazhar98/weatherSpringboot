d3eb0e6cf900996007e192575fbe13bf7e49e3f6
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-10 12:01:06 +0400

    Refactor weather prediction endpoint to simplify response.

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -5,6 +5,7 @@ import com.weatherapp.Models.weatherResponse;
 import com.weatherapp.sevices.WeatherPredictionService;
 import com.weatherapp.sevices.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,23 +34,10 @@ public class weatherController {
         }
     }
 
-    @GetMapping("/getPredictWeather/{city}")
-    public String getWeatherPrediction(@PathVariable String city) {
-        WeatherForecastResponse forecastResponse = weatherPredictionService.getWeatherForecast(city);
-        StringBuilder forecastOutput = new StringBuilder();
-
-        forecastResponse.getList().stream()
-                .filter(forecastItem -> forecastItem != null &&
-                        forecastItem.getDt_txt() != null &&
-                        forecastItem.getDt_txt().contains(DAILY_FORECAST_TIME))
-                .limit(3)
-                .forEach(forecastItem -> {
-                    if (forecastItem.getMain() != null) {
-                        forecastOutput.append("Temp: ").append(forecastItem.getMain().getTemp()).append("°C\n");
-                    }
-                });
-
-        return forecastOutput.toString();
+    @GetMapping("/forecast")
+    public ResponseEntity<String> getWeatherPrediction() {
+        String weatherPrediction = weatherPredictionService.getWeatherPrediction();
+        return ResponseEntity.ok(weatherPrediction);
     }
 
 }
