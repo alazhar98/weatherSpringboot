e47db74707630983f9ad2c87836ad2aeeb1550ce
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-10 14:41:06 +0400

    Add @Service annotation to WeatherService class

--- src/main/java/com/weatherapp/sevices/WeatherService.java
+++ src/main/java/com/weatherapp/sevices/WeatherService.java
@@ -2,12 +2,13 @@ package com.weatherapp.sevices;
 
 import com.weatherapp.Models.weatherResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@Service
 public class WeatherService {
 
     private static final Logger logger = LoggerFactory.getLogger(WeatherService.class);
