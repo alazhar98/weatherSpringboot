c0fb43424d59fd3934ff0ae0cc367d349671ca0d
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-11 11:34:59 +0400

    Refactor WeatherResponse model to simplify structure.

--- src/main/java/com/weatherapp/Models/WeatherResponse.java
+++ src/main/java/com/weatherapp/Models/WeatherResponse.java
@@ -1,28 +1,40 @@
 package com.weatherapp.Models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class WeatherResponse {
-    private Main main;
-    private String name;
-    private Weather[] weather;
-    private Sys sys;
+
+    private String city;
     private List<Forecast> list;
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Forecast {
+        private Main main;
+        private Weather[] weather;
+        private String dt_txt;
+
+
+        public double getTempMax() {
+            return main.getTempMax();
+        }
+
+        public double getTempMin() {
+            return main.getTempMin();
+        }
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Main {
         private double temp;
+        private double tempMax;
+        private double tempMin;
         private double humidity;
         private double pressure;
     }
@@ -32,42 +44,5 @@ public class WeatherResponse {
     public static class Weather {
         private String main;
         private String description;
-    }
-
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Sys {
-        @JsonIgnore
-        private long sunrise;
-        @JsonIgnore
-        private long sunset;
-        private String country;
-
-        @JsonProperty("sunriseTime")
-        public String getSunriseTime() {
-            return formatUnixTimestamp(sunrise);
-        }
-
-        @JsonProperty("sunsetTime")
-        public String getSunsetTime() {
-            return formatUnixTimestamp(sunset);
-        }
-    }
-
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Forecast {
-        private String dt_txt;
-        private Main main;
-        private Weather[] weather;
-
-        public String getFormattedDate() {
-            return formatUnixTimestamp(Instant.parse(dt_txt).getEpochSecond());
-        }
-    }
-
-    private static String formatUnixTimestamp(long timestamp) {
-        return ZonedDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.systemDefault())
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
