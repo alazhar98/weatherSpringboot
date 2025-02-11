d171baf739cafa92f17b1e07fa978ea04d7c19b9
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-11 10:19:39 +0400

    Fix missing city parameter in weather prediction service call

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -35,7 +35,7 @@ public class weatherController {
     @GetMapping("/forecast/{city}")
     public ResponseEntity<String> getWeatherPrediction(@PathVariable String city) {
         try {
-            String weatherPrediction = weatherPredictionService.getWeatherPrediction(); 
+            String weatherPrediction = weatherPredictionService.getWeatherPrediction(city);
             return ResponseEntity.ok(weatherPrediction);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
