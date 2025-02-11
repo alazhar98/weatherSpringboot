6e3ce7355b39f156eb806899cd62782621cce518
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-11 11:35:17 +0400

    Add daily forecast generation method to WeatherService

--- src/main/java/com/weatherapp/sevices/WeatherService.java
+++ src/main/java/com/weatherapp/sevices/WeatherService.java
@@ -1,7 +1,6 @@
 package com.weatherapp.sevices;
 
 import com.weatherapp.Models.WeatherResponse;
-;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
@@ -19,6 +18,7 @@ public class WeatherService {
 
     @Value("${weather.api.url}")
     public String apiUrl;
+
     public WeatherResponse getWeather(String city) {
 
         if (apiKey == null || apiKey.isEmpty()) {
@@ -28,20 +28,40 @@ public class WeatherService {
             throw new IllegalArgumentException("Weather API URL is missing.");
         }
 
-        String endPoint = String.format("%s?q=%s&appid=%s&units=metric", apiUrl, city, apiKey);
+        String endPoint = String.format("%s/forecast?q=%s&appid=%s&units=metric", apiUrl, city, apiKey);
         logger.info("Requesting weather data from: {}", endPoint);
 
         RestTemplate restTemplate = new RestTemplate();
 
         try {
+
             WeatherResponse response = restTemplate.getForObject(endPoint, WeatherResponse.class);
             if (response == null) {
                 throw new RuntimeException("No data received from API.");
             }
+
+
             return response;
+
         } catch (RestClientException e) {
             logger.error("Error fetching weather data: ", e);
             throw new RuntimeException("Failed to fetch weather data. Please check your API key or endpoint configuration.");
         }
+    }
+
+    public String getDailyForecast(WeatherResponse response) {
+        StringBuilder forecast = new StringBuilder();
+
+
+        for (int i = 0; i < 5; i++) {
+            WeatherResponse.Forecast forecastData = response.getList().get(i * 8);
+            forecast.append(String.format("Day %d: %s\nMax Temp: %.2f°C, Min Temp: %.2f°C\nWeather: %s\n\n",
+                    i + 1, forecastData.getDt_txt(),
+                    forecastData.getMain().getTempMax(), forecastData.getMain().getTempMin(),
+                    forecastData.getWeather()[0].getDescription()));
+        }
+
+        return forecast.toString();
+
     }
 }
