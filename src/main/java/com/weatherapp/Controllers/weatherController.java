5d5ec18f3defab2361dd24092a295af8445848e3
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-11 11:36:59 +0400

    Refactor WeatherController to remove prediction endpoint.

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -1,9 +1,8 @@
 package com.weatherapp.Controllers;
 
 import com.weatherapp.Models.WeatherResponse;
-import com.weatherapp.sevices.WeatherPredictionService;
+import com.weatherapp.sevices.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,30 +14,14 @@ public class weatherController {
     private WeatherService weatherService;
 
     @Autowired
-    private WeatherPredictionService weatherPredictionService;
-
 
     @GetMapping("/getWeather/{city}")
     public ResponseEntity<WeatherResponse> getWeather(@PathVariable String city) {
         try {
-
             WeatherResponse weatherResponse = weatherService.getWeather(city);
             return ResponseEntity.ok(weatherResponse);
         } catch (RuntimeException ex) {
             return ResponseEntity.badRequest().body(null);
-        }
-    }
-
-
-    @GetMapping("/forecast/{city}")
-    public ResponseEntity<String> getWeatherPrediction(@PathVariable String city) {
-        try {
-
-            String weatherPrediction = weatherPredictionService.getWeatherPrediction(city);
-            return ResponseEntity.ok(weatherPrediction);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error retrieving weather data: " + e.getMessage());
         }
     }
 }
