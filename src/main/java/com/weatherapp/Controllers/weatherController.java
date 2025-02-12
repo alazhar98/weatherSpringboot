71087bf07f1be652a80e7e37640192a9e6c630ae
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-12 08:45:21 +0400

    change

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -1,36 +1,29 @@
 package com.weatherapp.Controllers;
 
-import com.weatherapp.Models.WeatherResponse;
+
 import com.weatherapp.sevices.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
-@RequestMapping("/weather")
-
 public class weatherController {
-    @Autowired
-    private WeatherService weatherService;
+    private final WeatherService weatherService;
 
-    @GetMapping("/getWeather/{city}")
-    public ResponseEntity<WeatherResponse> getWeather(@PathVariable String city) {
-        try {
-            WeatherResponse weatherResponse = weatherService.getWeather(city);
-            return ResponseEntity.ok(weatherResponse);
-        } catch (RuntimeException ex) {
-            return ResponseEntity.badRequest().body(null);
-        }
+    @Autowired
+    public weatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
     }
 
-    @GetMapping("/forecast/{city}")
-    public ResponseEntity<String> getWeatherPrediction(@PathVariable String city) {
+
+    @GetMapping("/weather-forecast")
+    public ResponseEntity<String> getWeatherForecast(@RequestParam long cityId) {
         try {
-            WeatherResponse weatherResponse = weatherService.getWeather(city);
-            String forecast = weatherService.getDailyForecast(weatherResponse);
+            String forecast = weatherService.getWeatherForecastById(cityId);
             return ResponseEntity.ok(forecast);
-        } catch (RuntimeException ex) {
-            return ResponseEntity.badRequest().body("Error fetching forecast: " + ex.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error fetching weather data: " + e.getMessage());
         }
     }
 }
