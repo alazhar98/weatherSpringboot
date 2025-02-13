28373815fb0748449d4294943a15fc1a3cba80a1
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-13 11:50:35 +0400

    Method to get the wind direction from the degree of the wind

--- src/main/java/com/weatherapp/Services/WeatherService.java
+++ src/main/java/com/weatherapp/Services/WeatherService.java
@@ -90,7 +90,7 @@ public class WeatherService {
             default -> "🌍";
         };
     }
-
+    // Method to get the wind direction from the degree of the wind
     private String getWindDirection(int degrees) {
         String[] directions = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
         return directions[(int) Math.round(((double) degrees % 360) / 22.5) % 16];
