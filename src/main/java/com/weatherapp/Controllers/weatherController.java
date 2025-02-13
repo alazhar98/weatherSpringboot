7f15f0ad192bc8b1f74b06bb7cff8d7852d7d9e5
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-13 11:57:38 +0400

    UPDATE weatherController

--- src/main/java/com/weatherapp/Controllers/weatherController.java
+++ src/main/java/com/weatherapp/Controllers/weatherController.java
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-//class as a REST controller
+
 public class weatherController {
-    // Injecting the WeatherService
+
     private final WeatherService weatherService;
 
-    //based injection of the WeatherService
+
     @Autowired
     public weatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
     }
 
-    // Mapping HTTP GET request
+
     @GetMapping("/weather-forecast")
     public ResponseEntity<String> getWeatherForecast(@RequestParam Long cityId ) {
         try {
-            // Call the service to get the weather forecast for a given city
+
             String forecast = weatherService.getWeatherForecastById(cityId);
-            // Return the forecast as a successful response
+
             return ResponseEntity.ok(forecast);
         } catch (Exception e) {
             // In case of error, return an internal server error
