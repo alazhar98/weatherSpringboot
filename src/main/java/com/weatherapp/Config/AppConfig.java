afca98a0a42b0c2631422326c777b6185b070b1d
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-13 11:18:48 +0400

    configuration class

--- src/main/java/com/weatherapp/Config/AppConfig.java
+++ src/main/java/com/weatherapp/Config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+//  configuration class
 public class AppConfig {
 
     @Bean
