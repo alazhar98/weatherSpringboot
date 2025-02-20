5f2a15238418690b1ffa4d75756477338217ad51
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-20 09:55:42 +0400

    delete spaces

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-// Wait for the DOM to be fully loaded before executing the script
+
 document.addEventListener('DOMContentLoaded', function () {
     let buttonPress = document.getElementById('btn');
     if (buttonPress) {
