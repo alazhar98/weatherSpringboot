c5599bf1a7d37495a72f266e381401a0a6fa9ef2
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-20 15:15:45 +0400

    update

--- src/main/java/com/weatherapp/Config/SecurityConfig.java
+++ src/main/java/com/weatherapp/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.weatherapp.Config;
 
+import com.weatherapp.Filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
