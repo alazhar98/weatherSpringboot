a7bf5daa38ecd7689d1d682efd92ff68339979bc
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-23 14:57:13 +0400

    update

---
+++ src/main/java/com/weatherapp/Controllers/AuthControllers.java
@@ -0,0 +1,40 @@
+package com.weatherapp.Controllers;
+
+import com.weatherapp.DTOs.SignInRequestDTO;
+import com.weatherapp.DTOs.SignUpRequestDTO;
+import com.weatherapp.Services.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthControllers {
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDTO dto) {
+        try {
+            authService.signUp(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "User registered!"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(@Valid @RequestBody SignInRequestDTO dto) {
+        try {
+            String token = authService.signIn(dto);
+            return ResponseEntity.ok(Map.of("token",token));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+        }
+    }
+}
