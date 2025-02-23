42877f4329de7b56877b62b1ffe5d9496d4b9996
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-23 14:57:42 +0400

    update

---
+++ src/main/java/com/weatherapp/Services/AuthService.java
@@ -0,0 +1,52 @@
+package com.weatherapp.Services;
+
+import com.weatherapp.Configs.UserRepository;
+import com.weatherapp.DTOs.SignInRequestDTO;
+import com.weatherapp.DTOs.SignUpRequestDTO;
+import com.weatherapp.Utils.JwtUtil;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.weatherapp.Models.User;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    // Sign-in method for JWT generation
+    public String signIn(SignInRequestDTO dto) throws Exception {
+        // Find user by username
+        User user = userRepository.findByUserName(dto.getUsername());
+        // Check password match
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            throw new Exception("Invalid username or password");
+        }
+
+        // Generate JWT token
+        return jwtUtil.generateToken(user.getUserName(), user.getEmail(), user.getRole().name());
+    }
+    public void signUp(SignUpRequestDTO dto) throws Exception {
+        if (userRepository.findByUserName(dto.getUsername()) != null) {
+            throw new Exception("Username already exists");
+        }
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new Exception("Email already exists");
+        }
+
+        userRepository.save(
+                User.builder()
+                        .email(dto.getEmail())
+                        .password(passwordEncoder.encode(dto.getPassword()))
+                        .role(dto.getRole())
+                        .userName(dto.getUsername()).build()
+        );
+    }
+}
