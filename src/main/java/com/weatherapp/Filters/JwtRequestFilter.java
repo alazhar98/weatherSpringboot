13d35741454b9679b8aae396db00e89ce254d1e4
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-23 11:54:59 +0400

    update

--- src/main/java/com/weatherapp/Filters/JwtRequestFilter.java
+++ src/main/java/com/weatherapp/Filters/JwtRequestFilter.java
@@ -16,7 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-public class JwtRequestFilter {
+public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
