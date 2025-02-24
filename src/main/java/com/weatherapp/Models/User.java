39f19496f3edd56024c0979ec723238ddfa01641
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-02-24 10:49:18 +0400

    update

--- src/main/java/com/weatherapp/Models/User.java
+++ src/main/java/com/weatherapp/Models/User.java
@@ -8,10 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 
 @Entity
-@Builder
-@Data
-public class User {
 
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generate the ID
     private Long id; // Primary key
@@ -32,4 +30,100 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
+    // Private constructor for the builder
+    private User(UserBuilder builder) {
+        this.id = builder.id;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.userName = builder.userName;
+        this.role = builder.role;
+    }
+
+    // Default constructor (needed for JPA)
+    public User() {
+    }
+
+    // Builder class
+    public static class UserBuilder {
+        private Long id;
+        private String email;
+        private String password;
+        private String userName;
+        private Role role;
+
+        public UserBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public UserBuilder setRole(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    // Static method to get a new builder instance
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
